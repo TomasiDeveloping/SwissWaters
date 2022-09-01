8f82c9cf6643141df7835be358a9382f451073f9
Tomasi - Developing <info@tomasi-developing.ch>
2022-09-01 14:42:14 +0200

    Implements cache

--- Client/src/app/services/station.service.ts
+++ Client/src/app/services/station.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {catchError, EMPTY, Observable, shareReplay} from "rxjs";
 import {Station} from "../models/station.model";
 
 @Injectable({
@@ -10,12 +10,46 @@ import {Station} from "../models/station.model";
 export class StationService {
 
   private readonly apiUrl = environment.apiUrl + 'stations/';
+  stationCache!: Observable<Station[]>;
+  expires!: number;
+
 
   constructor(private http: HttpClient) { }
 
   getCurrentStationsData(): Observable<Station[]> {
-    return this.http.get<Station[]>(this.apiUrl + 'GetLatestMeasurements')
+    if (this.getCacheStations()) {
+      return this.getCacheStations()!;
+    }
+    let observable = this.http.get<Station[]>(this.apiUrl + 'GetLatestMeasurements').pipe(
+      shareReplay(1),
+      catchError(_ => {
+        return EMPTY;
+      })
+    );
+    this.setCache(observable);
+    return observable;
+
   }
+
+  getCacheStations(): Observable<Station[]> | null {
+    let cacheStations = this.stationCache;
+
+    if (!cacheStations) {
+      return null;
+    }
+    if (this.expires <= Date.now()) {
+      return null;
+    }
+
+    return cacheStations;
+  }
+
+  setCache(value: Observable<Station[]>): void {
+    // Cache set 10Minutes
+    this.expires = Date.now() + 600000;
+    this.stationCache = value;
+  }
+
 
   getStationsWhitCantonsOnly(): Observable<Station[]> {
     return this.http.get<Station[]>(this.apiUrl);
